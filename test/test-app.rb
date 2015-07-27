b371a3e0a526b8a47d94e09536ba1cd0b37bfe7f
Takayoshi Kimura <takayoshi@gmail.com>
2015-07-27 17:53:24 +0900

    Add test and assemble script

---
+++ test/test-app.rb
@@ -0,0 +1,21 @@
+ENV['RACK_ENV'] = 'test'
+
+require './app.rb'
+require 'sinatra'
+require 'test/unit'
+require 'rack/test'
+
+class AppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_it_says_hello
+    get '/'
+    assert last_response.ok?
+    assert_equal 'hello', last_response.body
+  end
+
+end
