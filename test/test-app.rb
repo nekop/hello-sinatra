b1b6ade719ae1beb6f5f118c58f0da70c481016d
Takayoshi Kimura <takayoshi@gmail.com>
2016-04-26 15:02:46 +0900

    Fix \n

--- test/test-app.rb
+++ test/test-app.rb
@@ -15,7 +15,7 @@ class AppTest < Test::Unit::TestCase
   def test_it_says_hello
     get '/'
     assert last_response.ok?
-    assert_equal 'hello\n', last_response.body
+    assert_equal "hello\n", last_response.body
   end
 
 end
